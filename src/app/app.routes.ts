420699f6f8ddf8e362174ea3608236253444693c
KPASAMAMII <jsaaihnacio@gmail.com>
2025-09-05 13:09:16 -0400

    Ultima actualizacion
    05/09/2025 13:10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { Contacto } from './contacto/contacto';
+import { Descarga } from './descarga/descarga';
 
 export const routes: Routes = [
     {path:'',component:Inicio},
     {path:'inicio',component:Inicio},
     {path:'contacto',component:Contacto},
     //el resto de rutas añadir acá
-
+    {path:'descarga',component:Descarga},
 
 
     
---
+++ src/app/descarga/descarga.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+
+@Component({
+  selector: 'app-descarga',
+  imports: [Header, Footer],
+  templateUrl: './descarga.html',
+  styleUrl: './descarga.css'
+})
+export class Descarga {
+
+}
