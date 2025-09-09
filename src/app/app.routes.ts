097730895c297c2f54b8d85bc03ae5d865922d42
Tomiduuuu <tomas.allendesd@gmail.com>
2025-09-09 13:11:52 -0300

    Pagina de inicio modificada
    La pagina de inicio ya esta terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { Contacto } from './contacto/contacto';
+import { Registro } from './registro/registro';
+import { Horarios } from './horarios/horarios';
 
 export const routes: Routes = [
     {path:'',component:Inicio},
     {path:'inicio',component:Inicio},
     {path:'contacto',component:Contacto},
+    {path:'registro',component:Registro},
+    {path:'horarios',component:Horarios},
     //el resto de rutas añadir acá
 
 
